bb21d280d4edd5f0b085d6e0fa3dc8cdd0e5f172
DANIELPAD\danie <daniel183.shen@gmail.com>
2025-02-27 15:08:49 +0800

    0227 ds algo

--- data_structure/linked_list.c
+++ data_structure/linked_list.c
@@ -1,4 +1,39 @@
 // linked list include two things, (1).data and (2).pointer
+// also a kind of struct
 
 #include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+void insert(struct node **head, int value) {
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = value;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void display(struct node *head) {
+    struct node *temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct node *head = NULL;
+
+    insert(&head, 10);
+    insert(&head, 20);
+    insert(&head, 30);
+
+    display(head);
+
+    return 0;
+}
 
