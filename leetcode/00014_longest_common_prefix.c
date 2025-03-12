39c49397de1cc36a544a6dcfeeb44e00130dac11
DANIELPAD\danie <daniel183.shen@gmail.com>
2025-03-12 11:55:12 +0800

    0312 leetcode

--- leetcode/00014_longest_common_prefix.c
+++ leetcode/00014_longest_common_prefix.c
@@ -2,7 +2,17 @@
 
 #include <stdio.h>
 
-int main() {
-    
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize == 1) return strs[0]; // if there is only one string inside
+    int i = 0;
+    while (strs[0][i] != '\0') {
+        for (int j = 1; j < strsSize; j++) {
+            if (strs[j][i] != strs[0][i]) {
+                strs[0][i] = '\0';
+                return strs[0];
+            }
+        }
+        i++;
+    }
+    return strs[0];
 }
-    
