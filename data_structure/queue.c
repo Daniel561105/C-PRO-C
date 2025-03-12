dc6c8f0ff2cdae43df503489419d49328f4ef17f
DANIELPAD\danie <daniel183.shen@gmail.com>
2025-03-12 17:20:32 +0800

    0312 ds&algo

---
+++ data_structure/queue.c
@@ -0,0 +1,3 @@
+// the main idea is a opposite of stack
+// stack has to push thing at the top and take things at the top, which means first in last out
+// queue has to push and pop at different side
--- data_structure/tree.c
+++ data_structure/tree.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// tree is a structure which there is no cycle
-//         (1)
-//        /   \
-//      (2)   (3)
-//     /   \     \
-//   (4)   (5)   (6)
+// tree has no cycle 
 
-struct TreeNode
+struct tree
 {
     int val;
-    struct TreeNode* left;
-    struct TreeNode* right;
+    struct tree *left;
+    struct tree *right;
 };
 
+struct tree *new_node (int val) {
+    struct tree* node = (struct tree*)malloc(sizeof(struct tree));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+int main() {
+    struct tree* root = new_node(1);
+    root->left = new_node(2);
+    root->right = new_node(3);
+    root->left->left = new_node(4);
+    root->left->right = new_node(5);
+    root->right->left = new_node(6);
+    root->right->right = new_node(7);
+    printf("%d",root->val);
+    return 0;
+}
